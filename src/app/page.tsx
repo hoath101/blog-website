5b31141c873d60815ae5c09330351865d9012000
hoath101 <hammadhoath123@gmail.com>
2025-02-04 15:47:46 +0500

    solved build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,12 +20,12 @@ const Blog = async () => {
       <h1 className='text-center text-3xl font-semibold text-sky-950 mt-6 mb-4'>Welcome to Our Blog.</h1>
       <code className='absolute text-cyan-200 w-[600px] ml-10 mt-4 pr-4'>
         We are excited to share inspiring stories, helpful tips, and insightful articles with you.
-        Whether you’re here to learn something new, get inspired, or simply enjoy a good read, we’ve got you covered.
-        Here, you’ll find a range of topics from lifestyle and wellness to tech and travel, all crafted with your
+        Whether you are here to learn something new, get inspired, or simply enjoy a good read, we have got you covered.
+        Here, you will find a range of topics from lifestyle and wellness to tech and travel, all crafted with your
         interests in mind. Our goal is to create content that resonates with you and sparks meaningful conversations.
-        Stay tuned for our latest updates, and feel free to explore our archives. We can’t wait to share this journey with you!
+        Stay tuned for our latest updates, and feel free to explore our archives. We cannot wait to share this journey with you.
         <p className='mt-4 w-[800px] text-center'>
-        Don&apos;t forget to subscribe to our newsletter for the latest posts delivered directly to your inbox!
+        Don&apos;t forget to subscribe to our newsletter for the latest posts delivered directly to your inbox.
       </p>
       </code>
       <Image src="/images/blog-pic.jpg" alt='blog-pic' width={600} height={300}
@@ -38,7 +38,7 @@ const Blog = async () => {
               <Link key={slug} href={`/${data.slug}`}>
                 <div className=' shadow-lg rounded hover:scale-105 active:scale-100 transition text-center p-2 max-h-[4200px]'>
                   <Image src={data.imageUrl} alt="abc" height={400} width={300}
-                    className=' w-[400px] h-[245px]  '></Image>
+                    className='w-[400px] h-[245px]'></Image>
                   <div>
                     <h2 className='text-2xl mb-3 mt-2'>
                       {data.title}
